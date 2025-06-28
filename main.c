b66a9eae4ad3e4b484a92458c6a314b4f55746e8
Jacek Sawinski <jacek.sawinski.0905@gmail.com>
2025-06-28 23:50:48 +0200

    Added basic cli arguments

--- main.c
+++ main.c
@@ -1,8 +1,62 @@
-#include <stdio.h>
-#include <dirent.h>
+#include "main.h"
 
 int main(int argc, char* argv[]) {
 
+  int opt;
+
+  while ((opt = getopt(argc, argv, ":nd:t:v::SDRVh")) != -1) {
+    switch (opt) {
+      case 'n':
+        printf("Option -n (--no, --simulate) selected\n");
+        break;
+      case 'd':
+        printf("Option -d (--dir) selected with argument %s\n", optarg);
+        break;
+      case 't':
+        printf("Option -t (--target) selected with argument %s\n", optarg);
+        break;
+      case 'v':
+        printf("Option -v (--verbose) selected with optional argument %s\n", optarg);
+        break;
+      case 'S':
+        printf("Option -S (--stow) selected\n");
+        break;
+      case 'D':
+        printf("Option -D (--delete) selected\n");
+        break;
+      case 'R':
+        printf("Option -R (--restow) selected\n");
+        break;
+      case 'V':
+        printf("Option -V (--version) selected\n");
+        break;
+      case 'h':
+        printf("Option -h (--help) selected\n");
+        break;
+      case ':':
+        printf("Option needs a value\n");
+        break;
+      case '?':
+        printf("Unknown option: %c\n", optopt);
+        break;
+    }
+  }
+
+  return 0;
+  /*
+  printf("You have entered %d arguments:\n", argc);
+
+  for (int i = 0; i < argc; i++) {
+    printf("%s\n", argv[i]);
+  }
+  
+  return 0;
+  */
+}
+
+
+
+int listdir(char* array[], int size) {
   struct dirent *de; // Pointer for directory entry
   
   // opendir() returns a pointer of type DIR
@@ -13,20 +67,21 @@ int main(int argc, char* argv[]) {
     return 0;
   }
 
-  while ((de = readdir(dr)) != NULL)
-    printf("%s\n", de->d_name);
+  while ((de = readdir(dr)) != NULL) {
+    if (de->d_type != DT_DIR) {
+      printf("File is not a directory: %s, skipping...\n", de->d_name);
+      continue;
+    }
+
+    if (strcmp(de->d_name, ".") == 0 || strcmp(de->d_name, "..") == 0) {
+      continue;
+    }
+
+    printf("%s ", de->d_name);
+    printf("%d\n", de->d_type);
+  }
 
   closedir(dr);
   return 0;
 
-
-  /*
-  printf("You have entered %d arguments:\n", argc);
-
-  for (int i = 0; i < argc; i++) {
-    printf("%s\n", argv[i]);
-  }
-  
-  return 0;
-  */
 }
---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <unistd.h>
+
+
+int main(int argc, char* argv[]);
+
+int listdir(char* array[], int size);
+
+#endif // MAIN_H_
